95f3501307b921ea34a7f328a8b7e8fa9b18c5da
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-03-06 01:56:19 -0500

    Outline background exercises

--- 01_background.py
+++ 01_background.py
@@ -8,19 +8,74 @@ app = marimo.App(width="medium")
 def _(mo):
     mo.md(
         r"""
-        # Build Your Own LP Solver: Background
+        # **Build Your Own LP Solver: Background**
         This project is a step-by-step walkthrough of how to build your own LP solver.
     
-        This is the second notebook in the series and is an optional file filled with exercises on Linear Programming, Matrices, Python, and algorithm implementation.
+        This is the second notebook in the series and is an optional file filled with exercises on Linear Programming, Python, Matrices, and algorithm implementation. Although these exercises are optional, they are **highly recommended** to solidify your understanding of the concepts discussed in the main notebook.
         """
     )
     return
 
 
 @app.cell
+def _(mo):
+    mo.md(r"## **1. Linear Programming Exercises**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"### **1.1. Protein Shake Formulation**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## **2. Python Exercises**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"### **2.1. Python Basics**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## **3. NumPy Matrix Exercises**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"### **3.1. Matrix Operations**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## **4. Algorithm Exercises**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"### **4.1. Simplex Algorithm**")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"## File Imports")
+    return
+
+
+@app.cell
 def _():
     import marimo as mo
-    return (mo,)
+    import numpy as np
+    return mo, np
 
 
 if __name__ == "__main__":
